dc67a75f521a3ba8dcd5d0330b82e4dc14ee66c4
MortenRicki <MortenRicki@WIN-6G42QHJ4MBL>
2015-10-20 21:50:58 +0200

    Model klasser stort set færdig, Begyndt på GUI

---
+++ Model/Basket.java
@@ -0,0 +1,26 @@
+package Model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Morten Ricki Rasmussen 
+ */
+public class Basket {
+    private ArrayList<Product> items;
+    
+    public void addItem(Product item) {
+        items.add(item);
+    }
+    
+    public int calculateTotalAmout() {
+        int totalAmout = 0;
+        
+        for (int i = 0; i < items.size(); i++) {
+            totalAmout = totalAmout + items.get(i).getPrice();
+        }
+        
+        return totalAmout;
+    }
+    
+}
